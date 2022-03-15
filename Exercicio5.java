f3e443d1b77a8b7c563fca2be0f5102488cacb62
CaueC <caue.coube@fatec.sp.gov.br>
2022-03-15 01:04:58 +0000

    Salvando

---
+++ Exercicio5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class Exercicio5 {
+
+    public static void main(String [] args){
+
+        int num;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        num = leitura.nextInt();
+
+        while (num > 1){
+            if (num % 2 == 0){
+                num = num / 2;
+            }
+            else{
+                num = 3 * num + 1;
+            }
+
+        System.out.print(num + " -> ");
+        leitura.close();
+        
+            }
+        
+        }
+        
+    }
