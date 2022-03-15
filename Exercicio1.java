d7f9e3b072561fd7e397b34e0014c8422439aaac
CaueC <caue.coube@fatec.sp.gov.br>
2022-03-15 00:46:45 +0000

    Salvando

---
+++ Exercicio1.java
@@ -0,0 +1,10 @@
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        for (int x = 150; x <= 300; x++){
+            System.out.println("Valor: " + x);
+        }
+    }
+    
+}
